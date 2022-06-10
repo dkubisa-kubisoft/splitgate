882b11d18e4899060438c834162cf854e5e68f11
Don Kubisa <dk@kubisoft.co>
2022-06-10 10:51:59 -0400

    Added refresh label

--- web/src/app/challenge/challenge.component.ts
+++ web/src/app/challenge/challenge.component.ts
@@ -122,7 +122,7 @@ export class ChallengesComponent implements OnInit, AfterViewInit {
     {
       hours -= 12;
     }
-    
+
     return "Refreshed on " + (refreshDate.getMonth() + 1) + '/' + refreshDate.getDate() + '/' + refreshDate.getFullYear() + " " + hours + ":" + this.zeroPad(refreshDate.getMinutes(), 2) + " " + amPm;
   }
 
